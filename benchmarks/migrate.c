297cc35b9fac9e38cf7372979084f3834f3067e8
Renato Mancuso <rntmancuso@gmail.com>
2021-02-05 10:33:38 -0500

    benchmarks: migrate: add -s option to sample mix/max/avg

--- benchmarks/migrate.c
+++ benchmarks/migrate.c
@@ -7,6 +7,8 @@
 #include <sys/sysinfo.h>
 #include <sys/ioctl.h>
 
+#define ACCUM_SAMPLES 10000
+
 /* Functions to interact with CPU cycle counter */
 #define magic_timing_begin(cycles)\
 	do{								\
@@ -82,42 +84,85 @@ void long_function(void) {
 void set_realtime(int prio)
 {
 	struct sched_param sp;
-	
+
 	/* Initialize parameters */
 	memset(&sp, 0, sizeof(struct sched_param));
 	sp.sched_priority = prio;
-	
+
 	/* Attempt to set the scheduler for current process */
-	if (sched_setscheduler(0, SCHED_FIFO, &sp) < 0) {
+	if (0 && sched_setscheduler(0, SCHED_FIFO, &sp) < 0) {
 		perror("Unable to set SCHED_FIFO scheduler");
 		exit(EXIT_FAILURE);
 	}
 
 	cpu_set_t set;
 	CPU_ZERO(&set);
-	
+
 	/* default to CPU 1 by default */
 	CPU_SET(1, &set);
-	
+
 	if (sched_setaffinity(getpid(), sizeof(set), &set) == -1) {
 		perror("Unable to set CPU affinity.");
-		exit(EXIT_FAILURE);			
+		exit(EXIT_FAILURE);
 	}
-	
+
 }
 
-int main() {
+int main(int argc, char ** argv) {
 	unsigned long ts;
+	int opt;
 
+	/* Setting the task as a real-time task with max priority
+	 * induces a stall on when attempting an LRU drain that is
+	 * needed to initiate page migration. Let's not do that
+	 * here. */
 	int max_prio = sched_get_priority_max(SCHED_FIFO);
 	set_realtime(max_prio);
-	
+
+	unsigned long min = ~0UL;
+	unsigned long max = 0;
+	unsigned long sum = 0;
+	unsigned long cur_sample = 0;
+
+	unsigned long sampled = 0;
+
+	while ((opt = getopt(argc, argv, "s")) != -1) {
+		switch (opt) {
+		case 's':
+			sampled = 1;
+			break;
+		default: /* '?' */
+			fprintf(stderr, "Usage: %s -s\n",
+				argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
 	while(1) {
 		magic_timing_begin(&ts);
 		long_function();
 		magic_timing_end(&ts);
-		printf("Exec. took %d\n", ts);
-		//sleep(1);
+
+		if (sampled) {
+			if (ts < min)
+				min = ts;
+			if (ts > max)
+				max = ts;
+			sum += ts;
+			if(++cur_sample >= ACCUM_SAMPLES) {
+				printf("\nMax: \t%d\n"
+				       "Min: \t%d\n"
+				       "Avg: \t%lf\n", max, min,
+				       ((double)sum)/ACCUM_SAMPLES);
+				cur_sample = 0;
+				min = ~0UL;
+				max = 0;
+				sum = 0;
+			}
+		} else {
+			printf("Exec. took %d\n", ts);
+		}
+
 	}
 
 	return EXIT_SUCCESS;
